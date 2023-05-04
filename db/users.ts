26ba861be06e94bec61ee4631c591cc13d622ae0
DANBOI <44661312+DANBOI@users.noreply.github.com>
2023-05-04 16:37:24 +0900

    add  login handler

--- db/users.ts
+++ db/users.ts
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
-    salt: { type: String, select: false },
+    salt: { type: String, required: true, select: false },
     sessionToken: { type: String, select: false },
   },
 });
