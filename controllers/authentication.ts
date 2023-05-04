f8a060773538f5d5d7b490f1947daea17d6b4f13
DANBOI <44661312+DANBOI@users.noreply.github.com>
2023-05-04 14:52:27 +0900

    add createUser controller

---
+++ controllers/authentication.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createUser, getUserByEmail } from '../db/users';
+import { random, authentication } from '../helpers/index';
+
+export const register = async (req: express.Request, res: express.Response) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const exsitingUser = await getUserByEmail(email);
+
+    if (exsitingUser) return res.sendStatus(400);
+
+    const salt = random();
+    const user = await createUser({
+      username,
+      email,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log('error');
+    return res.sendStatus(400);
+  }
+};
